bfddc572c34a390ad79bc5a3948a5cfbd3efb142
sohel160 <sohelcmc53rd@gmail.com>
2026-04-30 17:58:46 +0600

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,72 @@
+export default {
+  async fetch(request) {
+
+    const url = new URL(request.url)
+
+    if (url.searchParams.get("token") !== "abc123") {
+      return new Response("Forbidden", { status: 403 })
+    }
+
+    const ua = request.headers.get("User-Agent") || ""
+
+    const allowedUA = [
+      "Clash",
+      "clash",
+      "ClashMeta",
+      "ClashforWindows",
+      "ClashX",
+      "Stash",
+      "FiClash"
+    ]
+
+    let allowed = false
+
+    for (const a of allowedUA) {
+      if (ua.includes(a)) {
+        allowed = true
+        break
+      }
+    }
+
+    if (!allowed) {
+      return new Response("404 Not Found", { status: 404 })
+    }
+
+    const proxies = `
+proxies:
+
+- name: proxy1
+  type: http
+  server: 203.76.108.22
+  port: 27271
+- name: proxy2
+  type: http
+  server: 203.76.112.4
+  port: 27271
+- name: proxy3
+  type: http
+  server: 203.76.115.9
+  port: 27271
+- name: proxy4
+  type: http
+  server: 203.76.123.23
+  port: 27271
+- name: proxy5
+  type: http
+  server: 27.147.195.16
+  port: 27271
+- name: proxy6
+  type: http
+  server: 203.76.126.16
+  port: 27271
+  
+`
+
+    return new Response(proxies, {
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    })
+
+  }
+}
